d2d44b339325ae475bd0e02325b14b56e7efa752
Nero <karta0706566@gmail.com>
2023-03-24 22:32:22 +0800

    feat: add user register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
 
 // 登入頁面
 router.get('/login', (req, res) => { res.render('login') })
@@ -8,10 +9,24 @@ router.get('/login', (req, res) => { res.render('login') })
 router.post('/login',)
 
 // 註冊頁面
-router.get('/register', (req, res) => {res.render('register')})
+router.get('/register', (req, res) => { res.render('register') })
 
 // 註冊處理
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
 
+  User.findOne({ email })
+    .then((user) => {
+      if (user) { res.render('register', { name, email, password, confirmPassword }) }
+      else {
+        const newUser = new User({ name, email, password })
+        newUser.save()
+          .then(() => res.redirect('/'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
 
 
 module.exports = router
