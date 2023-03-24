855db6bfe1ca91c8ddfb30957a6db5d10f40980b
Nero <karta0706566@gmail.com>
2023-03-24 23:37:29 +0800

    feat: add logout function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -29,5 +29,12 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// 登出
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) { return next(err) }
+  })
+  res.redirect('/users/login')
+})
 
 module.exports = router
