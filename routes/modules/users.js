7e0d3b57c3657d8701af0266851f6e5ac0aa6f18
Nero <karta0706566@gmail.com>
2023-03-26 16:16:27 +0800

    feat: add bcrypt in register

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/User')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // 登入頁面
 router.get('/login', (req, res) => { res.render('login') })
 
 // 登入處理
-router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login',failureFlash:true }))
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }))
 
 // 註冊頁面
 router.get('/register', (req, res) => { res.render('register') })
@@ -16,30 +17,33 @@ router.get('/register', (req, res) => { res.render('register') })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  
-  if (!name||!email||!password||!confirmPassword){
-    errors.push({message:'All of the blanks is required.'})
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All of the blanks is required.' })
   }
 
-  if (password !== confirmPassword){
-    errors.push({message:'The password is different.'})
+  if (password !== confirmPassword) {
+    errors.push({ message: 'The password is different.' })
   }
 
-  if (errors.length){
-    return res.render('register',{errors,name,email,password,confirmPassword})
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
   }
 
   User.findOne({ email })
     .then((user) => {
-      if (user) { 
-        errors.push({message:'This email had been registered.'})
-        return res.render('register', { errors,name, email, password, confirmPassword }) 
+      if (user) {
+        errors.push({ message: 'This email had been registered.' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
       }
-      const newUser = new User({ name, email, password })
-      return newUser.save()
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name, email, password: hash
+        }))
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
-      })
+    })
     .catch(err => console.log(err))
 })
 
@@ -48,7 +52,7 @@ router.get('/logout', (req, res) => {
   req.logout((err) => {
     if (err) { return next(err) }
   })
-  req.flssh('success_msg', 'You are logout now')
+  req.flash('success_msg', 'You are logout now')
   res.redirect('/users/login')
 })
 
